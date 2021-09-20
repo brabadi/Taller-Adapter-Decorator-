f08e3ca8c116d8ed5aa420a4cb6527790ba2929a
brabadi <74464904+brabadi@users.noreply.github.com>
2021-09-20 16:24:34 -0500

    Add files via upload

---
+++ TallerAdapterDecorator-main/src/main/java/co/edu/unicauca/commandrestaurant/domain/FoodTypeEnum.java
@@ -0,0 +1,10 @@
+package co.edu.unicauca.commandrestaurant.domain;
+
+/**
+ * Tipos de comida del restaurante
+ *
+ * @author Astrid Carolina Cruz, Braian ALexis Bastidas
+ */
+public enum FoodTypeEnum {
+    ENTRADA, PRINCIPIO, CARNE, JUGO, POSTRE;
+}
